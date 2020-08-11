8e8831eace353d3f348d2986eb4715092b9f4d65
KibuKuhn <kibu.kuhn@gmail.com>
2020-08-11 15:04:11 +0200

    keyboard handling improved

--- src/main/java/kibu/kuhn/myfavorites/ui/XEventQueue.java
+++ src/main/java/kibu/kuhn/myfavorites/ui/XEventQueue.java
@@ -2,7 +2,10 @@ package kibu.kuhn.myfavorites.ui;
 
 import java.awt.AWTEvent;
 import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.util.function.Consumer;
+import javax.swing.JDialog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,11 +22,38 @@ class XEventQueue extends EventQueue {
   @Override
   protected void dispatchEvent(AWTEvent event) {
     try {
+      if (checkEsc(event)) {
+        doClose(event);
+      }
       super.dispatchEvent(event);
     }
     catch (Throwable th) {
       LOGGER.error(th.getMessage(), th);
       exceptionHandler.accept(th);
     }
+  }
+
+  private void doClose(AWTEvent event) {
+    if (!(event.getSource() instanceof JDialog)) {
+      return;
+    }
+    
+    JDialog dialog = (JDialog) event.getSource();
+    WindowEvent windowEvent = new WindowEvent(dialog, WindowEvent.WINDOW_CLOSING);
+    dialog.dispatchEvent(windowEvent);
+    
+  }
+
+  private boolean checkEsc(AWTEvent event) {
+    if (!(event instanceof KeyEvent)) {
+      return false;
+    }
+    
+    KeyEvent ke = (KeyEvent) event;
+    if (ke.getKeyCode() != KeyEvent.VK_ESCAPE) {
+      return false;
+    }
+    
+    return true;
   }
 }
--- src/main/java/kibu/kuhn/myfavorites/ui/buttonbar/ButtonBarButton.java
+++ src/main/java/kibu/kuhn/myfavorites/ui/buttonbar/ButtonBarButton.java
@@ -2,6 +2,9 @@ package kibu.kuhn.myfavorites.ui.buttonbar;
 
 import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.Action;
 import javax.swing.JButton;
 
@@ -16,6 +19,7 @@ class ButtonBarButton extends JButton {
       setBorder(null);
       setContentAreaFilled(false);
       setPreferredSize(new Dimension(32,32));
+      addKeyListener(new EnterAction());
     }
 
     @Override
@@ -23,4 +27,21 @@ class ButtonBarButton extends JButton {
       insets.bottom = insets.left = insets.right = insets.top = 2;
       return insets;
     }
+    
+    private class EnterAction extends KeyAdapter {
+
+      @Override
+      public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() != KeyEvent.VK_ENTER) {
+          return;
+        }
+        performAEnterction();
+      }
+      
+    }
+
+    private void performAEnterction() {
+      getAction().actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, getActionCommand()));
+    }
+    
   }
